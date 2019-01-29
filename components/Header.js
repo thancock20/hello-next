276775fcbf6ece552a40a8323ae3a3c244ffc333
Todd Hancock <thancock20@hotmail.com>
2019-01-28 21:57:49 -0600

    Add prefetch to links (#7)

--- components/Header.js
+++ components/Header.js
@@ -6,40 +6,40 @@ const linkStyle = {
 
 const Header = () => (
   <header>
-    <Link href="/">
+    <Link prefetch href="/">
       <a style={linkStyle}>Home</a>
     </Link>
-    <Link href="/about">
+    <Link prefetch href="/about">
       <a style={linkStyle}>About</a>
     </Link>
-    <Link href="/counter">
+    <Link prefetch href="/counter">
       <a style={linkStyle}>Counter</a>
     </Link>
-    <Link href="/counter-u1">
+    <Link prefetch href="/counter-u1">
       <a style={linkStyle}>Counter-U1</a>
     </Link>
-    <Link href="/counter-u2">
+    <Link prefetch href="/counter-u2">
       <a style={linkStyle}>Counter-U2</a>
     </Link>
-    <Link href="/counter-res">
+    <Link prefetch href="/counter-res">
       <a style={linkStyle}>Counter-RES</a>
     </Link>
-    <Link href="/counter-res1">
+    <Link prefetch href="/counter-res1">
       <a style={linkStyle}>Counter-RES1</a>
     </Link>
-    <Link href="/counter-res2">
+    <Link prefetch href="/counter-res2">
       <a style={linkStyle}>Counter-RES2</a>
     </Link>
-    <Link href="/counter-hooks">
+    <Link prefetch href="/counter-hooks">
       <a style={linkStyle}>Counter-Hooks</a>
     </Link>
-    <Link href="/counter-ezpz1">
+    <Link prefetch href="/counter-ezpz1">
       <a style={linkStyle}>Counter-EZPZ1</a>
     </Link>
-    <Link href="/counter-ezpz2">
+    <Link prefetch href="/counter-ezpz2">
       <a style={linkStyle}>Counter-EZPZ2</a>
     </Link>
-    <Link href="/tir-hooks">
+    <Link prefetch href="/tir-hooks">
       <a style={linkStyle}>TIR-Hooks</a>
     </Link>
   </header>
